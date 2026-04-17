c4e8a879c63eb80f49a79b6f2c17698354fd0e4e
justinrestrepo_325 <restrepojustin31@gmail.com>
2026-04-17 16:23:36 -0400

    fix: lines 89

--- TrainingApp/src/main/java/com/codebricks/controllers/PerformanceDashboardController.java
+++ TrainingApp/src/main/java/com/codebricks/controllers/PerformanceDashboardController.java
@@ -87,7 +87,7 @@ public class PerformanceDashboardController {
     private void handleBack() {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/login-view.fxml"));
-            // TODO: change to home-dashboard-view.fxml once Matt's screen is merged
+            // TODO: fix asap: update back button to navigate to home dashboard instead of login-view
             Scene scene = new Scene(loader.load(), 800, 600);
             scene.getStylesheets().add(
                     getClass().getResource("/styles/styles.css").toExternalForm()
